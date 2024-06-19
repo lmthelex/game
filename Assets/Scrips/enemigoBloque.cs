a0b9882bcb408e40733fa1b49b39bf5d11eb26c3
CosmicCooter <oscarlau.734@gmail.com>
2024-06-18 19:28:26 -0500

    Daño de bala
    si disparas 3 veces a un pincho, desaparece

--- Assets/Scrips/enemigoBloque.cs
+++ Assets/Scrips/enemigoBloque.cs
@@ -4,12 +4,26 @@ using UnityEngine;
 
 public class enemigoBloque : MonoBehaviour
 {
+    private int collisionCount = 0; // Contador de colisiones
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.transform.CompareTag("Player"))
         {
             Debug.Log("Player Damage");
             Destroy(collision.gameObject);
+        }
+        else if (collision.transform.CompareTag("Bullet"))
+        {
+            Debug.Log("Bullet Hit");
+            Destroy(collision.gameObject); // Destruir el proyectil
+
+            collisionCount++; // Incrementar el contador de colisiones
+            if (collisionCount > 2)
+            {
+                Debug.Log("Block Destroyed");
+                Destroy(gameObject); // Destruir el bloque enemigo
+            }
         }
     }
 }
