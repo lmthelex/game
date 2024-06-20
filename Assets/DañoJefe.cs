a2fd8928c092f26ea74689feaa86704341653a9f
CosmicCooter <oscarlau.734@gmail.com>
2024-06-20 17:50:11 -0500

    disparo
    Se recuperó la versión. Se puede disparar nuevamente, el disparo hace daño. Se agregó un jefe que avanza hacia la derecha

---
+++ Assets/DañoJefe.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+public class DañoJefe : MonoBehaviour
+{
+    private void Update()
+    {
+        gameObject.transform.Translate(0.005f,0,0) ;
+    }
+
+
+    private int collisionCount = 0; // Contador de colisiones
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.transform.CompareTag("Player"))
+        {
+            Debug.Log("Player Damage");
+            Destroy(collision.gameObject);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    { 
+        
+        if (other.CompareTag("Bullet"))
+        {
+            Debug.Log("Bullet Hit");
+
+            collisionCount++; // Incrementar el contador de colisiones
+            if (collisionCount > 2)
+            {
+                Debug.Log("Block Destroyed");
+                Destroy(gameObject); // Destruir el bloque enemigo
+            }
+        }
+    }
+}
